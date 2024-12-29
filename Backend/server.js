0286f7a4a3f24216271e052cfd94e23998d55ef6
Deepak Gupta <154800280+deepak-gupta-git@users.noreply.github.com>
2024-12-30 01:32:10 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-
 dotenv.config();
 const app = express();
+
+app.use(cors({ origin: 'https://task-managament-app-api.vercel.app', 
+methods: "GET, POST, PUT, PATCH, DELETE, HEAD",
+credentials: true
+}));
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello from Backend");
@@ -14,20 +18,11 @@ app.get("/", (req, res) => {
 const authRoutes = require('./routes/auth');
 const taskRoutes = require('./routes/task');
 
-
-
-
-
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: 'https://task-managament-app-api.vercel.app', 
-methods: "GET, POST, PUT, PATCH, DELETE, HEAD",
-credentials: true
-}));
-
+app.use(express.urlencoded({extended:false}));
 
 // MongoDB Connection
-
 const mongoURI = process.env.MONGODB_URI; 
 mongoose.connect(mongoURI, 
     { useNewUrlParser: true, useUnifiedTopology: true }).then(() =>
