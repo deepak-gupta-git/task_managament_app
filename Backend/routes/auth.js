10ea136baebd41e4d6661213a4b28b5d569e18c0
deepakgupta <deepak.gupta18122003@gmail.com>
2024-12-30 00:43:30 +0530

    updating brcyptjs package

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
