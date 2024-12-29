607ab906b9794ffeb2ef418423ac28ba9168d7c3
Deepak Gupta <154800280+deepak-gupta-git@users.noreply.github.com>
2024-12-30 00:20:34 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: ' http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://task-managament-app.vercel.app/login', credentials: true }));
 
 
 // MongoDB Connection
